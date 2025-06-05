60d6f87443bbbeea020d7eb33b8bd8f7b309f44c
valtervalik <valtervaralik@gmail.com>
2025-06-05 12:19:54 -0400

    feat: :sparkles: Implement file compression cron job and processor for automated file handling

---
+++ src/files/cron/files-compression.cron.ts
@@ -0,0 +1,35 @@
+import { InjectQueue } from '@nestjs/bullmq';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { Queue } from 'bullmq';
+import { FilesQueueKeys } from '../constants/files-queue-keys.enum';
+import { FilesService } from '../files.service';
+
+@Injectable()
+export class FilesCompressionCron {
+  private readonly logger = new Logger(FilesCompressionCron.name);
+
+  constructor(
+    @InjectQueue(FilesQueueKeys.COMPRESSION)
+    private readonly filesCompressionQueue: Queue,
+    private readonly filesService: FilesService,
+  ) {}
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async handleFileCompression() {
+    this.logger.debug('Starting file compression processing');
+
+    const filesToCompress = await this.filesService.findAllWithoutPagination({
+      status: 'raw',
+    });
+
+    if (!filesToCompress.total) {
+      this.logger.debug('No files to compress');
+      return;
+    }
+
+    await this.filesCompressionQueue.add(FilesQueueKeys.COMPRESSION, {
+      files: filesToCompress.elements,
+    });
+  }
+}
